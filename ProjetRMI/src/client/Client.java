9ff8e33e43b4d29c7bcce05af128bb4bb5ea2187
omar <omar.l.95@hotmail.fr>
2019-02-16 17:50:27 +0100

    exo 1 2 3 fini

--- ProjetRMI/src/client/Client.java
+++ ProjetRMI/src/client/Client.java
@@ -4,6 +4,7 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 
 import gestion.Banque;
+import gestion.AccessToken;
 
 public class Client {
 	
@@ -18,14 +19,46 @@ public class Client {
 			
 			registre = LocateRegistry.getRegistry();
 			// on recupere le registre
-/*
+
 			Banque instance_adapte = (Banque) registre.lookup("AccesBanqueDistance");
-			System.out.println("creation 1er compte : " + instance_adapte.creerCompte("nom_1", "prenom_1", 2000));
-			System.out.println("solde actuel : " + instance_adapte.getSolde(1));
-			instance_adapte.depot(1, 200);
-			System.out.println("solde actuel : " + instance_adapte.getSolde(1));
-*/
 			
+			
+			//creation 1 er compte et 1er token
+			AccessToken at = instance_adapte.creerCompte("nom_1", "prenom_1", 2000);
+			at.setIdentifiants("identifiant_1","mdp_1");
+			int id_compte=instance_adapte.registerToken(at);
+			
+			if(id_compte==-1)
+				System.out.println("Le serveur a un probleme avec le token recu");
+			
+			System.out.println("compte "+id_compte+" solde actuel : " + instance_adapte.getSolde(id_compte,at));
+			
+			instance_adapte.depot(id_compte, 200,at);
+			
+			System.out.println("compte "+id_compte+" solde actuel : " + instance_adapte.getSolde(id_compte,at));
+
+			
+			
+			//creation 2eme compte et 2eme token
+
+			AccessToken at2 = instance_adapte.creerCompte("nom_2", "prenom_2", 1000);
+			at2.setIdentifiants("identifiant_2","mdp_2");
+			int id_compte2=instance_adapte.registerToken(at2);
+			
+			if(id_compte2==-1)
+				System.out.println("Le serveur a un probleme avec le token recu");
+			
+			System.out.println("compte "+id_compte2+" solde actuel : " + instance_adapte.getSolde(id_compte2,at2));
+			
+			instance_adapte.depot(id_compte2, 200,at2);
+			
+			System.out.println("compte "+id_compte2+" solde actuel : " + instance_adapte.getSolde(id_compte2,at2));
+
+			
+			// test anti vol ! on prend 1er compte & 2eme token
+			
+			System.out.println("compte "+id_compte+" solde actuel : " + instance_adapte.getSolde(id_compte,at2));
+
 			
 		}
 		catch(Exception e) {
--- ProjetRMI/src/objetsDistants/BanqueImpl.java
+++ ProjetRMI/src/objetsDistants/BanqueImpl.java
@@ -1,5 +1,6 @@
 package objetsDistants;
 
+import java.rmi.RemoteException;
 import java.util.HashMap;
 import java.util.Random;
 
@@ -23,16 +24,19 @@ public class BanqueImpl implements Banque{
 	
 	@Override
 	public AccessToken creerCompte(String nom, String prenom, double soldeInitial) {
+
+		System.out.println("creation compte ....( sans liaison serveur auth )");
 		
 		int compte_id = comptes.size()+1;
 
-		comptes.put(comptes.size(),new Compte());
-		
+		comptes.put(compte_id,new Compte());
 		comptes.get(compte_id).setSolde(soldeInitial);
 		comptes.get(compte_id).setNom(nom);
 		comptes.get(compte_id).setPrenom(prenom);
 		comptes.get(compte_id).setIdCompte(compte_id);
 		
+		System.out.println("creation compte OK ( sans liaison serveur auth )");
+		System.out.println("ajout dans nos hashtable....");
 
 		Random rn = new Random();
 		Integer banque_id = rn.nextInt();
@@ -40,6 +44,9 @@ public class BanqueImpl implements Banque{
 		token_compte.put(idToken_class, compte_id);
 		token_banque.put(idToken_class, banque_id);
 		
+		System.out.println("ajout dans nos hashtable OK");
+		System.out.println("Serveur banque attend le retour token du client ...");
+
 		
 		AccessToken a = new AccessToken(idToken_class, compte_id, banque_id);
 		
@@ -52,36 +59,71 @@ public class BanqueImpl implements Banque{
 	
 	@Override
 	public Boolean depot(int idCompte, double montant, AccessToken token) {
+		try {
+			if (authServeur.verifierClef(idCompte,token))
+				return (comptes.get(idCompte)).depot(idCompte, montant);
+			else
+				System.out.println("Probleme token pour depot()");
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
 		
-		return (comptes.get(idCompte)).depot(idCompte, montant);
-		
+		return false;
 	}
 	
 	@Override
 	public Boolean retrait(int idCompte, double montant, AccessToken token) {
-		return (comptes.get(idCompte)).retrait(idCompte, montant);
+		try {
+			if (authServeur.verifierClef(idCompte,token))
+				return (comptes.get(idCompte)).retrait(idCompte, montant);
+			else
+				System.out.println("Probleme token pour retrait()");
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+		
+		return false;
 	}
 	
 	@Override
-	public Boolean virement(int idCompte, int idCompte2, double montant, AccessToken token) {
+	public Boolean virement(int idCompte, int idCompte2, double montant, AccessToken token1, AccessToken token2) {
 		
-		(comptes.get(idCompte2)).depot(idCompte2, montant);
-		return (comptes.get(idCompte)).retrait(idCompte, montant);
+		try {
+			if (authServeur.verifierClef(idCompte,token1) && authServeur.verifierClef(idCompte2,token2))
+				return ((comptes.get(idCompte2)).depot(idCompte2, montant) && (comptes.get(idCompte)).retrait(idCompte, montant));
+			else
+				System.out.println("Probleme token pour virement()");
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
 		
+		return false;
+
 	}
 	
 	@Override
 	public double getSolde(int idCompte, AccessToken token) {
 		
-		return (comptes.get(idCompte)).getSolde();
+		try {
+			if (authServeur.verifierClef(idCompte,token))
+				return (comptes.get(idCompte)).getSolde();
+			else 
+				System.out.println("Probleme token pour getSolde()");
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
 		
+		return -1;	
 	}
 	
 	@Override
 	public int registerToken(AccessToken token) {
 		
+		System.out.println("Serveur banque recoit token du client ... On lance les verifications");
+
 		int id_token = token.getIdToken();
-		int id_compte;
+		int id_compte=-1;
+		
 		 // on verifie que ce token existe
 		if(token_compte.get(id_token)!=null && token_banque.get(id_token)!=null)
 		{
@@ -89,26 +131,26 @@ public class BanqueImpl implements Banque{
 			if ( token.validateCleBanque(token_banque.get(id_token)))
 			{
 				try {// on enregistre sur authServeur et on vide nos hashmap !
-					id_compte=token_compte.get(token);
+					id_compte=token_compte.get(token.getIdToken());
 					authServeur.link(id_compte,token);
 					token_compte.remove(id_token);
 					token_banque.remove(id_token);
+					System.out.println("Verifications OKon vide nos hashtable OK");
+					System.out.println("on vide nos hashtable ... OK");
 				}catch(Exception e){
 					e.printStackTrace();
 				}
 
 			}
-			
+			else
+				System.out.println("Le serveur banque a un probleme avec le token passé ! token a un mauvais id");
 		}
 		else
-		{
-			System.out.println("tentative de hack ! on quitte le serveur");
-			System.exit(-1);
+			System.out.println("Le serveur banque a un probleme avec le token passé ! le token n'existe pas");
 
-		}
-		
+		System.out.println("Serveur banque : creation compte OK ( avec liaison serveur auth )");
+
 		return id_compte;
-		//return token.getIdToken();
 	}
 
 }
